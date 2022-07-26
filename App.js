7d35291c74d8e5e13132481c862eba5cb1470b0f
Jonathan <reachjonbrand@gmail.com>
2022-07-26 09:35:54 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,24 @@
+import React from 'react';
 
+var perf =require('./App.html');
+
+class App extends React.Component {
+   render(){
+      return (
+        <div>
+            <div class="carousel">
+                <div class="carousel-item">
+                    <img src={require("./1.png")} class="responsive-img"></img>
+                </div>
+                <div class="carousel-item">
+                    <img src={require("./2.png")} class="responsive-img"></img>
+                </div>
+                <div class="carousel-item">
+                    <img src={require("./3.png")} class="responsive-img"></img>
+                </div>  
+             </div>
+        </div>      
+      );
+      }
+      }
+    export default App;
